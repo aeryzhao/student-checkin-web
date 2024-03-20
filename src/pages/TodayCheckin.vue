fbcc988e2d9de99f0d87a82234c662cb812e4aa7
aeryzhao <iszhaoxg@outlook.com>
2024-03-20 13:57:18 +0800

    :sparkles: 删除学生

--- src/pages/TodayCheckin.vue
+++ src/pages/TodayCheckin.vue
@@ -1,17 +1,26 @@
 <template>
     <div>
-        <el-input v-model="name" style="width: 300px" placeholder="请输入学生姓名" />
+        <el-input v-model="name" style="width: 320px" placeholder="请输入学生姓名" />
         <el-button type="primary" @click="addStudent">添加</el-button>
     </div>
     <div>
         <el-table :data="students" border style="width: 100%">
             <!-- <el-table-column prop="id" label="ID" width="180" /> -->
             <el-table-column prop="name" label="姓名" width="180" />
-            <el-table-column prop="state" label="打卡" width="180">
-            <template #default="scope">
-                <el-button type="success" :disabled= 'scope.row.state != 0' @click="check(scope.row, 1)">✅</el-button>
-                <el-button type="danger" :disabled= 'scope.row.state != 0' @click="check(scope.row, 2)">❌</el-button>
-            </template>
+            <el-table-column prop="state" label="打卡" width="100">
+                <template #default="scope">
+                    <el-button type="success" :disabled='scope.row.state != 0'
+                        @click="check(scope.row, 1)">✅</el-button>
+                </template>
+            </el-table-column>
+            <el-table-column label="操作" width="100">
+                <template #default="scope">
+                    <el-popconfirm title="Are you sure to delete this student?" @confirm="deleteStudent(scope.row.id)">
+                        <template #reference>
+                        <el-button type="danger">删除</el-button>
+                    </template>
+                    </el-popconfirm>
+                </template>
             </el-table-column>
         </el-table>
     </div>
@@ -19,6 +28,7 @@
 
 <script>
 import axios from 'axios'
+import { ElMessage } from 'element-plus'
 export default {
     data() {
         return {
@@ -31,6 +41,12 @@ export default {
         this.getStudents()
     },
     methods: {
+        deleteStudent(id) {
+            axios.delete('/api/student/' + id).then(res => {
+                this.getStudents()
+            })
+            ElMessage.success('删除成功！')
+        },
         addStudent() {
             axios.post('/api/student', {
                 name: this.name
