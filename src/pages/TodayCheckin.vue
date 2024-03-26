c28085a835d8f88f3a4c18916c196ad1ec3dad73
zhaoxg <iszhaoxg@outlook.com>
2024-03-26 19:01:05 +0800

    调整样式

--- src/pages/TodayCheckin.vue
+++ src/pages/TodayCheckin.vue
@@ -3,18 +3,19 @@
         <el-input v-model="name" style="width: 350px" placeholder="请输入学生姓名" />
         <el-button type="primary" @click="addStudent">添加</el-button>
     </div>
+    <br>
     <div>
         <el-table :data="students" border style="width: 100%">
             <!-- <el-table-column prop="id" label="ID" width="180" /> -->
-            <el-table-column prop="name" label="姓名" width="180" />
-            <el-table-column prop="state" label="打卡" width="130">
+            <el-table-column prop="name" label="姓名" width="110" />
+            <el-table-column prop="state" label="打卡" width="150">
                 <template #default="scope">
                     <el-button type="success" :disabled='scope.row.state == 1'
                         @click="check(scope.row, 1)">✅</el-button>
                     <el-button type="danger" :disabled='scope.row.state == 2' @click="check(scope.row, 2)">❌</el-button>
                 </template>
             </el-table-column>
-            <el-table-column label="操作" width="100">
+            <el-table-column label="操作" width="90">
                 <template #default="scope">
                     <el-popconfirm title="Are you sure to delete this student?" @confirm="deleteStudent(scope.row.id)">
                         <template #reference>
